0029f7d66917b151d8b551c9b0f1a81822f212f9
BillySwCodeNinjas <122191321+BillySwCodeNinjas@users.noreply.github.com>
2023-07-29 12:22:09 -0500

    Full Game (no PY)

---
+++ BS- CyberFu Part 2/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int currentPlayerHealth = 100;
+    public int enemyDamage = 5; 
+    private Animator playerAnimator;
+    public PlayerExplosionParticles particles;
+   
+    void Start()
+    {
+       playerAnimator = GetComponent<Animator>();
+       particles = GetComponent<PlayerExplosionParticles>();
+    }
+    
+    public void HurtPlayer(){
+        currentPlayerHealth -= enemyDamage;
+        playerAnimator.SetTrigger("Hit");
+
+        if (currentPlayerHealth <= 0){
+            particles.Explode();
+            Invoke("ReloadScene", 5);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other){
+        if (other.tag == "HitCollider"){
+            HurtPlayer();
+        }
+    }
+
+    private void ReloadScene(){
+        SceneManager.LoadScene("CyberFu");
+    }
+}
